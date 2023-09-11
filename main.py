06f8d8d8608b0a6fd125e7853d32df95adedf403
Alessandro Ciocchetti <63201940+PaleAlex@users.noreply.github.com>
2023-09-11 16:59:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ with col1:
 
 if calcola_portfolio:
     ticker_stocks_list = ticker_stocks.upper().strip().split(",")
-    start = datetime.now()-timedelta(days=820)
+    start = datetime.now()-timedelta(days=1875)
     with col2:
         with st.spinner(text="Calcolando..."):
             tester = mc(ticker_stocks_list, start, end)
